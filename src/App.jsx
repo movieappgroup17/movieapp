479b8110d102dce071fe73aea84914230ed1a48a
Janina Niemelä <janina7590@hotmail.com>
2025-09-08 23:28:17 +0300

    React project initialized and created base for app

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Home from './pages/Home'
+import Favorites from './pages/Favorites'
+import Groups from './pages/Groups'
+import NotFound from './pages/NotFound'
+import Profile from './pages/Profile'
+import Reviews from './pages/Reviews'
+import Search from './pages/Search'
+import Showtimes from './pages/Showtimes'
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    <Header/>
+    <div id="container">
+      <Routes>
+        <Route path="/" exact element={<Home/>}/>
+        <Route path="/favorites" exact element={<Favorites/>}/>
+        <Route path="/groups" exact element={<Groups/>}/>
+        <Route path="/*" exact element={<NotFound/>}/>
+        <Route path="/profile" exact element={<Profile/>}/>
+        <Route path="/reviews" exact element={<Reviews/>}/>
+        <Route path="/search" exact element={<Search/>}/>
+        <Route path="/showtimes" exact element={<Showtimes/>}/>
+      </Routes>
+    </div>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
