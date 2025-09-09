43efc8408a3c72bf8b3748671a13c26c6e4f491d
Samistus <40127394+Samistus@users.noreply.github.com>
2025-09-09 21:17:21 +0300

    simple search function

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
@@ -12,14 +12,64 @@ import Profile from './pages/Profile'
 import Reviews from './pages/Reviews'
 import Search from './pages/Search'
 import Showtimes from './pages/Showtimes'
+import ReactPaginate from 'react-paginate'
 
 function App() {
+  const [movies, setMovies] = useState([])
+  const [page, setPage] = useState (1)
+  const [pageCount, setPageCount] = useState (0)
+  const [query, setQuery] = useState('Star wars')
+
+  const Movies = () =>{
+    return (
+      <table>
+        <tbody>
+        { movies && movies.map(movie => (
+          <tr key={movie.id}><td>{movie.title}</td></tr>
+        ))}
+        </tbody>
+      </table>
+    )
+  } 
+
+  const search = () =>{
+      fetch('https://api.themoviedb.org/3/search/movie?query=' + query + ' &include_adult=false&language=en-US&page=' + page,{
+      headers:{
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYTJlNTY4M2NhYWFjZTEyN2FmMDQxNThhYTQ0NjIxZCIsIm5iZiI6MTc1NzQyMTQ1OS4xNjcsInN1YiI6IjY4YzAxZjkzMzI4NTRmMDZkYzljMGM5MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dSBy_osKCyQGYn2qlaIhVtop1BAykVrrgDpiBlIuVG8',
+        'Content-Type' : 'application/json'
+      }
+    })
+    .then (response => response.json())
+    .then(json =>{
+      //console.log(json)
+      setMovies(json.results)
+      setPageCount(json.total_pages)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+  useEffect(() =>{
+    search()
+  }, [page])
 
   return (
     <>
     <Navbar/>
     <Header/>
     <div id="container">
+      <h3>Search movies: </h3>
+      <input value={query} onChange={e => setQuery(e.target.value)}></input><button onClick={search} type="button">Search</button>
+      <ReactPaginate
+      breakLabel="..."
+      nextLabel=">"
+      onPageChange={(e) => setPage(e.selected + 1)}
+      pageRangeDisplayed={5}
+      pageCount={pageCount}
+      previousLabel="<"
+      renderOnZeroPageCount={null}
+      />
+      <Movies/>
       <Routes>
         <Route path="/" exact element={<Home/>}/>
         <Route path="/favorites" exact element={<Favorites/>}/>
@@ -35,5 +85,6 @@ function App() {
     </>
   )
 }
+
 
 export default App
