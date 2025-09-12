16506f3eb283280198bc3e7ce4db95c82fc1f2f1
Samistus <40127394+Samistus@users.noreply.github.com>
2025-09-12 19:24:59 +0300

    Combined search functions, added movie posters to search, did some styling to search and pagination

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,42 +1,56 @@
 import { useState } from 'react'
 import { SearchBar } from '../components/SearchBar.jsx'
 import { discoverMovies, searchMoviesByText } from '../components/SearchFunctions'
+import ReactPaginate from 'react-paginate'
 
 function Search() {
   const [results, setResults] = useState([])
   const [loading, setLoading] = useState(false)
   const [err, setErr] = useState(null)
+  const [page, setPage] = useState(1);
+  const [pageCount, setPageCount] = useState(0);
+  const defaultFilters = {
+     genres: [],
+     year: "",
+     query: "",
+     sort: "popularity.desc"
+    }
+    const [filters, setFilters] = useState(defaultFilters)
 
-  const defaultFilters = { genres: [], year: "", query: "", sort: "popularity.desc" }
-
-  async function runSearch(filters) {
-    console.log("filters", filters)
+  async function runSearch(newFilters, newPage = 1) {
+    setFilters (newFilters)
+    setPage (newPage)
+    //console.log("filters", filters)
     setLoading(true)
     setErr(null)
 
     try {
       let data;
-      const hasFilters = filters.genres.length || filters.year || filters.sort !== "popularity.desc"
-      if (filters.query && hasFilters) {
+      const hasFilters = newFilters.genres.length || newFilters.year || newFilters.sort !== "popularity.desc"
+      if (newFilters.query && hasFilters) {
         // Jos on tekstihaku + suodattimia, voi käyttää discoveria ilman querya
         // yhdistäminen: hae searchilla ja suodata clientissä 
         // Tässä valitaan discover, koska TMDB ei yhdistä suoraan query + with_genres
         data = await discoverMovies({
-          genres: filters.genres,
-          year: filters.year,
-          sort: filters.sort
+          genres: newFilters.genres,
+          year: newFilters.year,
+          sort: newFilters.sort,
+          page: newPage
         })
-      } else if (filters.query) {
-        data = await searchMoviesByText({ query: filters.query })
+      } else if (newFilters.query) {
+        data = await searchMoviesByText({ query: newFilters.query, page: newPage})
       } else {
         data = await discoverMovies({
-          genres: filters.genres,
-          year: filters.year,
-          sort: filters.sort
+          genres: newFilters.genres,
+          year: newFilters.year,
+          sort: newFilters.sort,
+          page: newPage
         }
         );
       }
       setResults(data?.results ?? [])
+      setPageCount(data?.total_pages ?? 0)
+      setPage (newPage)
     } catch (e) {
       setErr(e);
     } finally {
@@ -54,11 +68,33 @@ function Search() {
       <div className="grid gap-3 mt-4">
         {results.map((m) => (
           <div key={m.id} className="border p-3 rounded">
+          {m.poster_path && (
+        <img
+          src={`https://image.tmdb.org/t/p/w200${m.poster_path}`}
+          alt={m.title}
+        />
+      )} 
             <div className="font-semibold">{m.title}</div>
             <div>{m.release_date}</div>
           </div>
         ))}
       </div>
+
+
+      {pageCount > 1 &&(
+        <ReactPaginate
+        breakLabel="..."
+        nextLabel=">"
+        onPageChange={(e) => runSearch(filters, e.selected + 1)}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel="<"
+        renderOnZeroPageCount={null}
+        containerClassName='pagination'
+        activeClassName='selected'
+        forcePage={page -1}
+        />
+      )}
     </div>
   );
 }
