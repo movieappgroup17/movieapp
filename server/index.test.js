f55f78421ee0f4f0e2b10d54ef6635cb2972c7da
Janina Niemelä <janina7590@hotmail.com>
2025-10-08 21:37:38 +0300

    Added testing for browsing reviews

--- server/index.test.js
+++ server/index.test.js
@@ -1,7 +1,8 @@
 import { expect } from "chai"
-import { initializeTestDb, insertTestUser, getToken } from "./helper/test.js"
+import { initializeTestDb, insertTestUser, getToken, addTestReview, addTestMovie } from "./helper/test.js"
 import sinon from "sinon"
 import { performLogout } from '../src/utils/logout.js'
+import { getAllReviews } from "./controller/reviewController.js"
 
 /*describe("Testing database", () => {
     let token = null
@@ -26,6 +27,7 @@ describe("Testing user management", () => {
     const newUser = {email: "test.user@test.com", password: "Password123", nickname: "Nickname1"}
     // token for account deletion
     let deleteToken
+
 
     before(() => {
         insertTestUser(user)    // insert testuser to database
@@ -111,7 +113,7 @@ describe("Testing logout in Frontend", () => {
         setUser = sinon.spy() 
         toast = { success: sinon.spy()}
         // debug to check in console that user is set correctly
-        console.log(sessionStorage.getItem('user'))
+        //console.log(sessionStorage.getItem('user'))
     })
 
     // testing logout
@@ -135,5 +137,63 @@ describe("Testing logout in Frontend", () => {
         // check toast.success is called once and message includes "logged out"
         expect(toast.success.calledOnce).to.be.true
         expect(toast.success.firstCall.args[0]).to.include("logged out")
+    })
+})
+
+// testing reviews
+describe("Testing browsing reviews", () => {  
+    
+    
+    const firstMovie = {
+            movieID: 8587,
+            title: "The Lion King",
+            date: "1994-06-15",
+            text: "Young lion prince Simba, eager to one day become king of the Pride Lands, grows up under the watchful eye of his father Mufasa; all the while his villainous uncle Scar conspires to take the throne for himself. Amid betrayal and tragedy, Simba must confront his past and find his rightful place in the Circle of Life.",
+            imageURL: "/sKCr78MXSLixwmZ8DyJLrpMsd15.jpg"
+        }
+
+        const secondMovie = {
+            movieID: 14160,
+            title: "Up",
+            date: "1994-06-15",
+            text: "Carl Fredricksen spent his entire life dreaming of exploring the globe and experiencing life to its fullest. But at age 78, life seems to have passed him by, until a twist of fate (and a persistent 8-year old Wilderness Explorer named Russell) gives him a new lease on life.",
+            imageURL: "/mFvoEwSfLqbcWwFsDjQebn9bzFe.jpg"
+        }
+        const firstReview = {
+            movieID: 8587,
+            userID: 2,
+            stars: 3,
+            text: "It was OK"
+        }
+        const secondReview = {
+            movieID: 14160,
+            userID: 2,
+            stars: 5,
+            text: "Best movie ever!!!"
+        }
+
+    before( async () => {
+        
+        try {
+            await addTestMovie(firstMovie)
+            await addTestMovie(secondMovie)
+            await addTestReview(firstReview)
+            await addTestReview(secondReview)
+        } catch (error) {
+            console.error("Error inserting test data:", error)
+            throw error
+        }
+        
+    })
+
+    it("should get all reviews", async () => {
+        const response = await fetch("http://localhost:3001/reviews", {
+            method: "get",
+            headers: { "Accept": "application/json" }
+        })
+        const data = await response.json()  // parse server response as JSON
+        expect(data.length).to.equal(2)
+        expect(data[0]).to.include.all.keys(["movieid", "userid", "stars", "text"])  // check response contains all expected keys
+        expect(data[1]).to.include.all.keys(["movieid", "userid", "stars", "text"])  // check response contains all expected keys
     })
 })
